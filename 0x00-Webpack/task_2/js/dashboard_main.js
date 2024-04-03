a3dbaef6c5ea016bbe9b7be5bb312ca1bea6e3fd
Polaris-algedi <karchalmohammed@gmail.com>
2024-04-03 02:39:46 +0000

    Adding CSS & Images

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,31 @@
+import $ from "jquery";
+
+// Import jQuery
+
+// Create elements
+const paragraph1 = $("<p>").text("Holberton Dashboard");
+const paragraph2 = $("<p>").text("Dashboard data for the students");
+const button = $("<button>").text("Click here to get started");
+const countParagraph = $("<p>").attr("id", "count");
+const copyrightParagraph = $("<p>").text("Copyright - Holberton School");
+
+// Append elements to the body
+$("body").append(
+  paragraph1,
+  paragraph2,
+  button,
+  countParagraph,
+  copyrightParagraph
+);
+
+// Counter variable
+let count = 0;
+
+// Function to update counter
+function updateCounter() {
+  count++;
+  $("#count").text(`${count} clicks on the button`);
+}
+
+// Bind debounce function to click event
+button.on("click", _.debounce(updateCounter, 300));
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css",
+    }),
+  ],
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
+  },
+};
