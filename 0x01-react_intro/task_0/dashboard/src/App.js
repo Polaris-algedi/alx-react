576e1cd200fb1875591880412ed7faf475aabb6b
Polaris-algedi <karchalmohammed@gmail.com>
2024-04-03 12:42:30 +0000

    Add a comment

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
+/*
+Images are not loading.
+https://stackoverflow.com/questions/34582405/react-wont-load-local-images
 
+When using Webpack you need to require images in order for Webpack
+to process them, which would explain why external images load while
+internal do not, so instead of <img src={"/images/resto.png"} />
+you need to use <img src={require('/images/image-name.png')} />
+replacing image-name.png with the correct image name for each of them.
+That way Webpack is able to process and replace the source img.
+*/
 function Header() {
   return (
     <div className="App-header">
